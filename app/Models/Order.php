09c005741b69d3ecf94f9d3c1086db9c8e4b49cd
fayas-dev <fayas.emedia@gmail.com>
2021-03-21 08:46:54 +0530

    order model updated

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use App\Models\Advertisement;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -26,11 +27,52 @@ class Order extends Model
     ];
 
     protected $visible = ['id','customer_id','advertisement_id','order_details','description_1','description_2','description_3'
-        ,'status','add_line_1','add_line_2','add_line_3','latitude','longitude'];
+        ,'status','add_line_1','add_line_2','add_line_3','latitude','longitude','name','last_name','phone','add_line_1','add_line_2','add_line_3'];
+
+    protected $appends = ['name','last_name','phone','add_line_1','add_line_2','add_line_3'];
 
 
     public function advertisement()
     {
         return $this->belongsTo(Advertisement::class);
     }
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->customer->name;
+    }
+
+    public function getLastNameAttribute()
+    {
+        return $this->customer->last_name;
+    }
+
+    public function getPhoneAttribute()
+    {
+        return $this->customer->phone;
+    }
+
+    public function getAddLine1Attribute()
+    {
+        return $this->customer->add_line_1;
+    }
+
+    public function getAddLine2Attribute()
+    {
+        return $this->customer->add_line_2;
+    }
+
+    public function getAddLine3Attribute()
+    {
+        return $this->customer->add_line_3;
+    }
+
+
+
+
 }
